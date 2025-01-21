4222d7124442de002cc2ebafea81aafd555696cf
Rob van der Lee <robvdlee@outlook.com>
2025-01-21 17:18:22 +0100

    Added a variant which uses an entire TCP stream to get the features The test is done simply by checking twice (once with the src port and once with the dst port)

---
+++ scripts/get_unique_features_streams.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+import os
+import pyshark
+import csv
+import argparse
+from entry import get_entry
+
+# Function to extract the source, destination, and protocol information
+def extract_data(pcap_dir, output_csv, num_lines):
+    pcap_files = [f for f in os.listdir(pcap_dir) if f.endswith('.pcapng')]
+
+    # Set to store unique tuples
+    ip_streams = []
+    unique_entries = {}
+
+    # Loop through the files
+    for pcap_file in pcap_files:
+        file = os.path.join(pcap_dir, pcap_file)
+        print("Opening {0}".format(file))
+        # Open the pcapng file using pyshark
+        cap = pyshark.FileCapture(file)
+        print("Opened {0}".format(file))
+
+        line = 0
+
+        # Loop through the packets in the pcapng file
+        for packet in cap:
+            eth_type = packet.eth.type
+            eth_src = packet.eth.src
+            eth_dst = packet.eth.dst
+
+            # Skip host PC traffic
+            if eth_src == "0c:37:96:c3:0c:f3" or eth_src == "9c:7b:ef:76:62:66" or eth_src == "08:00:27:54:05:b6":
+                continue
+
+            protocol = packet.highest_layer
+            ip_src = "0.0.0.0"
+            ip_dst = "0.0.0.0"
+            ip_proto = '0'
+            ip_dst_port = '0'
+            ip_src_port = '0'
+
+            if 'IP' in packet:
+                ip_src = packet.ip.src
+                ip_dst = packet.ip.dst
+                ip_proto = packet.ip.proto
+
+                if 'TCP' in packet:
+                    # If this is a TCP packet then handle the entire stream
+                    ip_stream = packet.tcp.stream
+                    if ip_stream in ip_streams:
+                        continue
+                    ip_streams.append(ip_stream)
+
+                    #Set all the protocols to TCP for obvious reasons
+                    protocol = 'TCP'
+                    ip_dst_port = packet.tcp.dstport
+                    entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, ip_src_port, ip_dst_port)
+
+                    # Add entry when not existing
+                    if entry not in unique_entries:
+                        unique_entries[entry] = {"occurrences": 1,
+                                                 "capfiles": {pcap_file}}
+                        print(entry)
+
+                    #Add another entry, reversing it
+                    entry = (eth_type, eth_dst, eth_src, protocol, ip_dst, ip_src, ip_proto, ip_dst_port, ip_src_port)
+                    if entry not in unique_entries:
+                        unique_entries[entry] = {"occurrences": 1,
+                                                 "capfiles": {pcap_file}}
+                        print(entry)
+
+                    #Go to the next packet
+                    continue
+
+                elif 'UDP' in packet:
+                    # UDP traffic which is profinet does not have a fixed portnr
+                    if protocol != "PN_IO_DEVICE" and protocol != "PN_IO_CONTROLLER":
+                        ip_src_port = packet.udp.srcport
+                        ip_dst_port = packet.udp.dstport
+
+            entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, ip_src_port, ip_dst_port)
+
+            # Add entry when not existing
+            if entry not in unique_entries:
+                unique_entries[entry] = {"occurrences": 1,
+                                      "capfiles": {pcap_file}}
+                print(entry)
+            else:
+                entry = unique_entries[entry]
+                entry["occurrences"] += 1
+                if pcap_file not in entry["capfiles"]:
+                    entry["capfiles"].add(pcap_file)
+
+            # Logging and limitation
+            line += 1
+            if line % 50000 == 0:
+                print("{}, {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), line))
+            if line == num_lines:
+                break
+
+        # Close pcap
+        cap.close()
+
+    # Write the unique data to a CSV file
+    with open(output_csv, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["eth_type", "eth_src", "eth_dst", "protocol", "ip_src", "ip_dst", "ip_proto", "ip_src_port", "ip_dst_port", "occurrences", "capfiles"])
+        for feature, metadata in unique_entries.items():
+            row = feature + tuple({metadata["occurrences"]}) + tuple({'; '.join(metadata["capfiles"])})
+            writer.writerow(row)
+
+# Main function to handle command-line arguments
+def main():
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser(
+        description="Extract unique source, destination, and protocol from pcapng files")
+
+    # Add arguments
+    parser.add_argument('pcap_dir', type=str, help="Path to the pcapng directory")
+    parser.add_argument('output_csv', type=str, help="Path to the output CSV file")
+    parser.add_argument('num_lines', type=int, help="Number of lines (packets) to read from the pcap file")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the function with parsed arguments
+    extract_data(args.pcap_dir, args.output_csv, args.num_lines)
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/test_against_unique_features_streams.py
@@ -0,0 +1,157 @@
+import datetime
+import pyshark
+import csv
+import argparse
+from entry import get_entry
+
+consolidate = True
+consolidate_threshold_seconds = 1
+
+# Function to read CSV file and store entries in a set
+def read_csv(csv_file):
+    unique_entries = set()
+
+    with open(csv_file, mode='r') as file:
+        reader = csv.reader(file)
+        # Skip header
+        next(reader)
+        for row in reader:
+            entry = tuple(row[:9])  # Convert each row into a tuple for comparison
+            unique_entries.add(entry)
+
+    return unique_entries
+
+# Function to check pcap file entries not in the CSV
+def check_pcap_against_csv(pcap_file, csv_file, num_lines, output_csv):
+    # Read entries from the CSV file
+    csv_entries = read_csv(csv_file)
+
+    print(f"Opening {pcap_file}")
+    cap = pyshark.FileCapture(pcap_file)
+    print(f"Opened {pcap_file}")
+
+    ip_streams = []
+    anomalies = {}
+
+    line = 0
+
+    # Loop through the packets in the pcapng file
+    for packet in cap:
+        eth_type = packet.eth.type
+        eth_src = packet.eth.src
+        eth_dst = packet.eth.dst
+
+        # Skip host PC traffic
+        if eth_src == "0c:37:96:c3:0c:f3" or eth_src == "9c:7b:ef:76:62:66" or eth_src == "08:00:27:54:05:b6":
+            continue
+
+        protocol = packet.highest_layer
+        ip_src = "0.0.0.0"
+        ip_dst = "0.0.0.0"
+        ip_proto = '0'
+        ip_dst_port = '0'
+        ip_src_port = '0'
+
+        entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, ip_src_port, ip_dst_port)
+
+        if 'IP' in packet:
+            ip_src = packet.ip.src
+            ip_dst = packet.ip.dst
+            ip_proto = packet.ip.proto
+
+            if 'TCP' in packet:
+                # Set all the protocols to TCP for obvious reasons
+                protocol = 'TCP'
+                ip_src_port = packet.tcp.srcport
+                ip_dst_port = packet.tcp.dstport
+                entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, ip_src_port, '0')
+                if entry not in csv_entries:
+                    #What the HACK, but it works
+                    entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, '0', ip_dst_port)
+
+            elif 'UDP' in packet:
+                # UDP traffic which is profinet does not have a fixed portnr
+                if protocol != "PN_IO_DEVICE" and protocol != "PN_IO_CONTROLLER":
+                    ip_src_port = packet.udp.srcport
+                    ip_dst_port = packet.udp.dstport
+
+                entry = (eth_type, eth_src, eth_dst, protocol, ip_src, ip_dst, ip_proto, ip_src_port, ip_dst_port)
+
+        # Check if the entry from pcap is in the CSV entries
+        if entry not in csv_entries:
+            #At this point we know that the package is not in the unique features set
+            detected = False
+
+            if entry in anomalies:
+                #We have an exact match
+                detected = True
+                entry = anomalies[entry]
+                entry["occurrences"] += 1
+            elif consolidate:
+                #We are going to check for the same entry with differing ports
+                # If we find it and it is within a few second we consider it to be the same "anomaly"
+                entry_without_src_port = (entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6], entry[8])
+                entry_without_dst_port = (entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6], entry[7])
+                for anomaly, metadata in anomalies.items():
+                    anomaly_without_src_port = (anomaly[0], anomaly[1], anomaly[2], anomaly[3], anomaly[4], anomaly[5], anomaly[6], anomaly[8])
+                    anomaly_without_dst_port = (anomaly[0], anomaly[1], anomaly[2], anomaly[3], anomaly[4], anomaly[5], anomaly[6], anomaly[7])
+                    previous_sniff_time = metadata["sniff_time"]
+                    if packet.sniff_time < previous_sniff_time + datetime.timedelta(
+                            seconds=consolidate_threshold_seconds):
+                        if anomaly_without_dst_port == entry_without_dst_port:
+                            # Source is same
+                            detected = True
+                            metadata["src_port_same"] += 1
+                            metadata["sniff_time"] = packet.sniff_time
+                            break
+                        elif anomaly_without_src_port == entry_without_src_port:
+                            #Destination is same
+                            detected = True
+                            metadata["dst_port_same"] += 1
+                            metadata["sniff_time"] = packet.sniff_time
+                            break
+
+            if not detected:
+                anomalies[entry] = {"occurrences": 1,
+                                    "src_port_same": 0,
+                                    "dst_port_same": 0,
+                                    "sniff_time": packet.sniff_time}
+                print(entry)
+
+        line += 1
+        if line % 50000 == 0:
+            print("{}, {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), line))
+        if line == num_lines:
+            break
+
+    cap.close()
+
+    # Write the unique data to a CSV file
+    with open(output_csv, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["eth_type", "eth_src", "eth_dst", "protocol", "ip_src", "ip_dst", "ip_proto", "ip_src_port", "ip_dst_port", "occurrences", "src_port_same", "dst_port_same"])
+        for anomaly, metadata in anomalies.items():
+            row = anomaly + tuple({metadata["occurrences"]}) + tuple({metadata["src_port_same"]}) + tuple({metadata["dst_port_same"]})
+            writer.writerow(row)
+
+
+# Main function to handle command-line arguments
+def main():
+    parser = argparse.ArgumentParser(
+        description="Check for unique entries in a pcapng file that are not in the provided CSV file")
+
+    parser.add_argument('pcap_file', type=str, help="Path to the pcapng file")
+    parser.add_argument('csv_file', type=str, help="Path to the CSV file")
+    parser.add_argument('num_lines', type=int, help="Number of lines (packets) to read from the pcap file")
+    parser.add_argument('-o', type=str, help="CSV file to write anomalies to")
+
+    args = parser.parse_args()
+
+    if not args.o:
+        args.o = args.pcap_file.replace(".pcapng", ".csv")
+
+    check_pcap_against_csv(args.pcap_file, args.csv_file, args.num_lines, args.o)
+
+
+if __name__ == "__main__":
+    main()
